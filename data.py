63f1e02cead621b92e15e2cbd0c75bb79688ebb9
Daniel Nichols <dando18studios@gmail.com>
2022-05-05 15:09:57 -0400

    added cleaned up data readers

---
+++ data.py
@@ -0,0 +1,66 @@
+import os.path
+
+import numpy as np
+from scipy import sparse
+import pandas as pd
+import pickle
+import logging
+
+import rnaseqTools
+
+def get_mouse_exon_dataset():
+    PKL_FPATH = './data/mouse-exon.pkl'
+
+    if os.path.isfile(PKL_FPATH):
+        dataset = pickle.load(PKL_FPATH)
+    else:
+        fnames = ['./data/mouse_VISp_2018-06-14_exon-matrix.csv', 
+            './data/mouse_ALM_2018-06-14_exon-matrix.csv']
+        
+        datasets = [rnaseqTools.sparseload(fname) for fname in fnames]
+        
+        counts = sparse.vstack([c[0] for c in datasets])
+        cells = np.concatenate([c[2] for c in datasets])
+
+        for d in datasets:
+            gene = d[1]
+            assert np.all(gene == datasets[0][1])
+        genes = np.copy(datasets[0][1])
+
+        genesTable = pd.read_csv('./data/mouse_VISp_2018-06-14_genes-rows.csv')
+        ids = genesTable['gene_entrez_id'].tolist()
+        symbols = genesTable['gene_symbol'].tolist()
+        id2symbol = dict(zip(ids, symbols))
+        genes = np.array([id2symbol[g] for g in genes])
+
+        clusterInfo = pd.read_csv('data/tasic-sample_heatmap_plot_data.csv')
+        goodCells = clusterInfo['sample_name'].values
+        ids = clusterInfo['cluster_id'].values
+        labels = clusterInfo['cluster_label'].values
+        colors = clusterInfo['cluster_color'].values
+
+        clusterNames = np.array([labels[ids==i+1][0] for i in range(np.max(ids))])
+        clusterColors = np.array([colors[ids==i+1][0] for i in range(np.max(ids))])
+        clusters = np.copy(ids)
+
+        indices = np.array([np.where(cells==c)[0][0] for c in goodCells])
+        counts = counts[indices, :]
+
+        areas = (indices < datasets[0][2].size).astype(int)
+        clusters -= 1
+
+        dataset = {'counts': counts, 'genes': genes, 'clusters': clusters, 'areas': areas, 
+                'clusterColors': clusterColors, 'clusterNames': clusterNames}
+        
+        pickle.dump(dataset, open(PKL_FPATH, 'wb'))
+    
+    logging.info('Read data size with size: {}'.format(dataset['counts'].shape))
+    logging.info('Areas: {} {}'.format(np.sum(dataset['areas']==0), np.sum(dataset['areas']==1)))
+    logging.info('Unique clusters: {}'.format(np.unique(dataset['clusters']).size))
+
+    return dataset
+
+
+def get_dataset(name):
+    if name == 'mouse-exon':
+        return get_mouse_exon_dataset()
---
+++ main.py
@@ -0,0 +1,26 @@
+from data import get_dataset
+
+from argparse import ArgumentParser
+import logging
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--log', choices=['INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO', 
+        type=str.upper, help='logging level')
+    parser.add_argument('-d', '--dataset', type=str, choices=['synthetic', 'mouse-exon'])
+    args = parser.parse_args()
+
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: {}'.format(args.log))
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] -- %(message)s', 
+        filename='log.txt', filemode='w', level=numeric_level)
+
+    dataset = get_dataset(args.dataset)
+
+
+
+
+if __name__ == '__main__':
+    main()
