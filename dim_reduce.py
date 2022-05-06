98cfc9ed4bcd85f619e8d5b0eb0a5f89d67e34f6
Daniel Nichols <dando18studios@gmail.com>
2022-05-06 15:02:48 -0400

    added umap

--- dim_reduce.py
+++ dim_reduce.py
@@ -1,14 +1,13 @@
 import numpy as np
-from analysis_tools import preprocess
 
-def get_dim_reduction(ds, algorithm='pca', **kwargs):
+def get_dim_reduction(X, algorithm='pca', **kwargs):
 
     if algorithm == 'pca':
-        return pca(ds, **kwargs)
+        return pca(X, **kwargs)
     elif algorithm == 'tsne':
-        return tsne(ds, **kwargs)
+        return tsne(X, **kwargs)
     elif algorithm == 'umap':
-        return umap(ds, **kwargs)
+        return umap(X, **kwargs)
     else:
         raise NotImplementedError('{} not supported yet.'.format(algorithm))
 
@@ -19,24 +18,26 @@ def pca(ds):
     raise NotImplementedError('pca not supported yet.')
 
 
-def tsne(ds, perplexity=30, learning_rate='auto', init=None):
+def tsne(X, perplexity=30, learning_rate='auto', init='random'):
     ''' Compute the t-SNE dimensionality reduction.
     '''
     from openTSNE import TSNE
 
-    X = ds['counts']
-    X = preprocess(X, normalize=True, subsets=ds['markerSubset'])
-
     if learning_rate == 'auto':
         learning_rate = X.shape[0]/12
     
-    model = TSNE(perplexity=perplexity, learning_rate=learning_rate, initialization=init)
+    model = TSNE(perplexity=perplexity, learning_rate=learning_rate)
     reduced_data = model.fit(X)
 
     return reduced_data
 
 
-def umap(ds):
+def umap(X, n_neighbors=15, metric='euclidean'):
+    ''' UMAP API: https://umap-learn.readthedocs.io/en/latest/api.html
     '''
-    '''
-    raise NotImplementedError('UMAP not supported yet.')
+    from umap import UMAP
+
+    model = UMAP(n_neighbors=n_neighbors, metric=metric)
+    reduced_data = model.fit_transform(X)
+
+    return reduced_data
