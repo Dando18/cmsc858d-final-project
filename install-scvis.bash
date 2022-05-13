a3e29fa3a2961dbb060d5df7d00a856ce3f70635
Daniel Nichols <dando18studios@gmail.com>
2022-05-13 15:19:47 -0400

    add install helper script for scvis

--- install-scvis.bash
+++ install-scvis.bash
@@ -2,7 +2,7 @@
 
 # Upgrade scvis to support tensorflow 2
 cd scvis/lib/scvis
-tf_upgrade_v2 --intree . --outtree . --print_all
+#tf_upgrade_v2 --intree . --outtree . --print_all
 cd ../..
 
 # install
