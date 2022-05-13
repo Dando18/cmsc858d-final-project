9f567b840b7b08809425a4ad8edacd95c1e3db2e
Daniel Nichols <dando18studios@gmail.com>
2022-05-13 15:18:15 -0400

    added density parameter

--- dim_reduce.py
+++ dim_reduce.py
@@ -56,7 +56,7 @@ def umap(X, n_neighbors=15, metric='euclidean'):
     return reduced_data
 
 
-def densne(X):
+def densne(X, dens_lambda=0.1, dens_frac=0.3):
     ''' densne API: https://github.com/hhcho/densvis/tree/master/densne
         Run following command in densvis/densne subdirectory first.
             g++ sptree.cpp densne.cpp densne_main.cpp -o den_sne -O2
@@ -65,16 +65,16 @@ def densne(X):
     sys.path.append('./densvis/densne')
     from densne import run_densne
     
-    reduced_data = run_densne(X, verbose=True, final_dens=False)
+    reduced_data = run_densne(X, verbose=True, final_dens=False, dens_lambda=dens_lambda, dens_frac=dens_frac)
 
     return reduced_data
 
-def densmap(X, n_neighbors=15, metric='euclidean'):
+def densmap(X, n_neighbors=15, metric='euclidean', dens_lambda=2.0, dens_frac=0.3):
     ''' UMAP API: https://umap-learn.readthedocs.io/en/latest/api.html
     '''
     from umap import UMAP
 
-    model = UMAP(densmap=True, n_neighbors=n_neighbors, metric=metric)
+    model = UMAP(densmap=True, n_neighbors=n_neighbors, metric=metric, dens_lambda=dens_lambda, dens_frac=dens_frac)
     reduced_data = model.fit_transform(X)
 
     return reduced_data
@@ -83,12 +83,50 @@ def scvis(X):
     ''' scvis repo: https://github.com/shahcompbio/scvis
     '''
     from scvis.run import train
+    from dataclasses import dataclass
+    import tempfile
+    import os.path
+    from os import listdir
+    import pandas as pd
+
+    import tensorflow as tf
+    tf.compat.v1.disable_eager_execution()
+
+    @dataclass
+    class ScvisArgs:
+        data_matrix_file: str
+        out_dir: str
+        config_file: str = None
+        pretrained_model_file: str = None
+        data_label_file: str = None
+        normalize: bool = True
+        verbose: bool = False
+        verbose_interval: int = 100
+        show_plot: bool = False
+
+    reduced = None
+    #with tempfile.TemporaryDirectory() as tmpdir:
+    tmpdir = 'scvis-output'
+    mat_fname = os.path.join(tmpdir, 'data.tsv')
+
+    pd.DataFrame(X).to_csv(mat_fname, index=False, sep='\t')
+
+    args = ScvisArgs(mat_fname, tmpdir, None, None, None, True, True)
+    train(args)
+
+    output_fname = [filename for filename in listdir(tmpdir) if filename.startswith("perplexity_") and filename.endswith('tsv') and (not filename.endswith('likelihood.tsv'))][0]
+    output_fname = os.path.join(tmpdir, output_fname)
+    reduced = pd.read_csv(output_fname, sep='\t', usecols=['z_coordinate_0', 'z_coordinate_1'])
+
+    return reduced.to_numpy()
 
 
 def netsne(X):
     '''
     '''
     pass
+
+
 
 def hsne(X):
     '''
